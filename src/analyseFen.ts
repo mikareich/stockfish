576a061f7ee91fd2595229b79c51859838b39a2b
Mika Reich <mikareich0@gmail.com>
2023-06-10 12:26:21 +0200

    ?

---
+++ src/analyseFen.ts
@@ -0,0 +1,32 @@
+const Stockfish = require("stockfish.wasm");
+
+export default async function analyseFen(
+  fen: string,
+  depth: number
+): Promise<{ bestMove: string; evauluation: number }> {
+  const stockfish = await Stockfish();
+
+  stockfish.postMessage(`position fen ${fen}`);
+  stockfish.postMessage(`go depth ${depth}`);
+
+  return new Promise((resolve) => {
+    let bestMove: string, evauluation: number;
+
+    stockfish.addMessageListener((msg: any) => {
+      if (msg.includes("bestmove")) {
+        console.log(msg);
+        bestMove = msg.match(/bestmove (.*)/)?.[1].split(" ")[0];
+        stockfish.postMessage("eval");
+      }
+      if (msg.includes("Final evaluation:")) {
+        evauluation = Number(
+          msg.match(/Final evaluation:\s+(-?\d+(\.\d+)?)/)?.[1]
+        );
+      }
+
+      if (bestMove && evauluation) {
+        resolve({ bestMove, evauluation });
+      }
+    });
+  });
+}
---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import analyseFen from "./analyseFen";
+import { Chess } from "chess.js";
+
+const app = express();
+
+app.get("/analysis", async (req, res) => {
+  const { fen, depth } = req.query;
+
+  if (!fen) {
+    return res.status(400).json({ error: "Missing fen" });
+  }
+
+  // check if fen is valid with chess.js api
+  try {
+    new Chess(fen as string);
+  } catch (e) {
+    return res.status(400).json({ error: "Invalid fen" });
+  }
+
+  const analysis = await analyseFen(fen as string, Number(depth) || 10);
+
+  return res.json({
+    fen,
+    depth: Number(depth) || 10,
+    ...analysis,
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
